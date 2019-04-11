b197750100f4b7804cdac159029c3846bdc65b69
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-04-11 19:43:55 +0200

    Accumulate counters using "horizontal reduction"

--- pospopcnt.c
+++ pospopcnt.c
@@ -2500,6 +2500,8 @@ int pospopcnt_u16_avx512bw_adder_forest(const uint16_t* array, uint32_t len, uin
 int pospopcnt_u16_avx512bw_adder_forest(const uint16_t* data, uint32_t len, uint32_t* flags) { return(0); }
 #endif
 
+#define HANDVECTORIZED_SUM
+
 #if defined(__AVX512BW__) && __AVX512BW__ == 1
 int pospopcnt_u16_avx512bw_harvey_seal(const uint16_t* array, uint32_t len, uint32_t* flags) {
     for (uint32_t i = len - (len % (32 * 16)); i < len; ++i) {
@@ -2522,7 +2524,9 @@ int pospopcnt_u16_avx512bw_harvey_seal(const uint16_t* array, uint32_t len, uint
     const uint64_t limit = size - size % 16;
 
     uint16_t buffer[32];
-
+#ifdef HANDVECTORIZED_SUM
+    __m512i flagsv = _mm512_loadu_si512((const __m512i*)flags);
+#endif
     uint64_t i = 0;
     while (i < limit) {
         for (size_t i = 0; i < 16; ++i)
@@ -2558,13 +2562,68 @@ int pospopcnt_u16_avx512bw_harvey_seal(const uint16_t* array, uint32_t len, uint
         for (size_t i = 0; i < 16; ++i) U(i)
 #undef U
         
-        for (size_t i = 0; i < 16; ++i) {
-            _mm512_storeu_si512((__m512i*)buffer, counter[i]);
-            for (size_t z = 0; z < 32; z++) {
-                flags[i] += 16 * (uint32_t)buffer[z];
-            }
-        }
+#if defined(HANDVECTORIZED_SUM)
+#define HADD_UINT16(x)                                          \
+        _mm512_add_epi32(                                       \
+            _mm512_srli_epi32(x, 16),                           \
+            _mm512_and_epi32(x, _mm512_set1_epi32(0xffff)))
+    
+        counter[0x0] = HADD_UINT16(counter[0x0]);
+        counter[0x1] = HADD_UINT16(counter[0x1]);
+        counter[0x2] = HADD_UINT16(counter[0x2]);
+        counter[0x3] = HADD_UINT16(counter[0x3]);
+        counter[0x4] = HADD_UINT16(counter[0x4]);
+        counter[0x5] = HADD_UINT16(counter[0x5]);
+        counter[0x6] = HADD_UINT16(counter[0x6]);
+        counter[0x7] = HADD_UINT16(counter[0x7]);
+        counter[0x8] = HADD_UINT16(counter[0x8]);
+        counter[0x9] = HADD_UINT16(counter[0x9]);
+        counter[0xA] = HADD_UINT16(counter[0xA]);
+        counter[0xB] = HADD_UINT16(counter[0xB]);
+        counter[0xC] = HADD_UINT16(counter[0xC]);
+        counter[0xD] = HADD_UINT16(counter[0xD]);
+        counter[0xE] = HADD_UINT16(counter[0xE]);
+        counter[0xF] = HADD_UINT16(counter[0xF]);
+
+#define SHUFFLE_LO(a, b) \
+    _mm512_permutex2var_epi32(a, _mm512_setr_epi32(0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30), b)
+
+#define SHUFFLE_HI(a, b) \
+    _mm512_permutex2var_epi32(a, _mm512_setr_epi32(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31), b)
+
+#define REDUCE_UINT32(a, b) \
+    _mm512_add_epi32(SHUFFLE_LO(a, b), SHUFFLE_HI(a, b))
+    
+        counter[0x0] = REDUCE_UINT32(counter[0x0], counter[0x1]);
+        counter[0x1] = REDUCE_UINT32(counter[0x2], counter[0x3]);
+        counter[0x2] = REDUCE_UINT32(counter[0x4], counter[0x5]);
+        counter[0x3] = REDUCE_UINT32(counter[0x6], counter[0x7]);
+        counter[0x4] = REDUCE_UINT32(counter[0x8], counter[0x9]);
+        counter[0x5] = REDUCE_UINT32(counter[0xA], counter[0xB]);
+        counter[0x6] = REDUCE_UINT32(counter[0xC], counter[0xD]);
+        counter[0x7] = REDUCE_UINT32(counter[0xE], counter[0xF]);
+        
+        counter[0x0] = REDUCE_UINT32(counter[0x0], counter[0x1]);
+        counter[0x1] = REDUCE_UINT32(counter[0x2], counter[0x3]);
+        counter[0x2] = REDUCE_UINT32(counter[0x4], counter[0x5]);
+        counter[0x3] = REDUCE_UINT32(counter[0x6], counter[0x7]);
+        
+        counter[0x0] = REDUCE_UINT32(counter[0x0], counter[0x1]);
+        counter[0x1] = REDUCE_UINT32(counter[0x2], counter[0x3]);
+
+        counter[0x0] = REDUCE_UINT32(counter[0x0], counter[0x1]);
+
+        flagsv = _mm512_add_epi32(flagsv, _mm512_slli_epi32(counter[0x0], 4));
+#undef HADD_UINT16
+#undef SHUFFLE_LO
+#undef SHUFFLE_HI
+#undef REDUCE_UINT32
+#endif // defined(HANDVECTORIZED_SUM)
     }
+
+#ifdef HANDVECTORIZED_SUM
+    _mm512_storeu_si512((__m512*)flags, flagsv);
+#endif
 
     _mm512_storeu_si512((__m512i*)buffer, v1);
     for (size_t i = 0; i < 32; i++) {
